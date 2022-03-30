a9e0d3fec77e87e060463ba0411006a71d453f6b
Lucas Trentin <lucas.mtrentin@hotmail.com>
2022-03-30 11:45:23 -0300

    Add -> Events and Command Handlers

---
+++ commands/info/vitaoCorno.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "Victor",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        message.reply("corno")
+    }
+}
